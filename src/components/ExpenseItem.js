d2491efb9c13a8b42791f98a29bf82e2ef961f37
alexercolin <alex_ercolin@hotmail.com>
2021-08-17 08:27:37 -0300

    feat:add constants and formating date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const expenseYear = props.date.getFullYear();
+  const expenseMonth = props.date.toLocaleString("en-US", { month: "long" });
+  const expenseDay = props.date.toLocaleString("en-US", { day: "2-digit" });
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{expenseYear}</div>
+        <div>{expenseMonth}</div>
+        <div>{expenseDay}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
