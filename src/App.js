bb74fb7145b559123ad3c0eb1d47a9c7e5cac8f0
alexercolin <alex_ercolin@hotmail.com>
2021-09-17 09:06:45 -0300

    feat: create new dummy variable to clean the app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpenses from "./components/NewExpenses/NewExpenses";
 import ExpenseChart from "./components/ExpensesChart/ExpenseChart";
-
-const DUMMY_EXPENSES = [
-  { id: "e1", date: new Date(), title: "Car insurance", amount: 294.67 },
-  {
-    id: "e2",
-    date: new Date(2020, 5, 3),
-    title: "Toilet Paper",
-    amount: 5.99,
-  },
-  { id: "e3", date: new Date(2019, 8, 2), title: "New TV", amount: 1399.99 },
-  {
-    id: "e4",
-    date: new Date(2021, 10, 14),
-    title: "New desk",
-    amount: 499.0,
-  },
-];
+import DUMMY_EXPENSES from "./shared/constants/dummyData";
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
@@ -37,7 +21,7 @@ const App = () => {
         <NewExpenses onAddExpenses={newExpensesInput} />
         <Expenses data={expenses} />
         <ExpenseChart data={expenses} />
-x      </div>
+      </div>
     </>
   );
 };
---
+++ src/shared/constants/dummyData.js
@@ -0,0 +1,60 @@
+const DUMMY_EXPENSES = [
+  { id: "e1", date: new Date(), title: "Car insurance", amount: 294.67 },
+  {
+    id: "e2",
+    date: new Date(2021, 5, 3),
+    title: "Toilet Paper",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+  { id: "e3", date: new Date(2021, 8, 2), title: "New TV", amount: Math.round(Math.random() * (700 - 100 ) + 100),},
+  {
+    id: "e4",
+    date: new Date(2021, 10, 14),
+    title: "New desk",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+  {
+    id: "e5",
+    date: new Date(2021, 9, 19),
+    title: "New desk",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+  {
+    id: "e6",
+    date: new Date(2021, 8, 14),
+    title: "New desk",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+  {
+    id: "e7",
+    date: new Date(2021, 7, 8),
+    title: "New desk",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+  {
+    id: "e8",
+    date: new Date(2021, 6, 6),
+    title: "New desk",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+  {
+    id: "e8",
+    date: new Date(2021, 1, 15),
+    title: "New desk",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+  {
+    id: "e8",
+    date: new Date(2021, 3, 24),
+    title: "New desk",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+  {
+    id: "e8",
+    date: new Date(2021, 4, 28),
+    title: "New desk",
+    amount: Math.round(Math.random() * (700 - 100 ) + 100),
+  },
+];
+
+export default DUMMY_EXPENSES;
